550242eea1bd3f5eba2accd6126618031b08a0f7
好想吃鱼 <2178754128@qq.com>
2025-03-06 11:16:48 +0800

    第一次提交

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args){
+        int count=0;
+        for(int i=1;i<=10000;i++){
+            String str=i+"";
+            long a=(long)i*i*i;
+            String str2=a +"";
+            if(str2.endsWith(str))
+                count+=1;
+        }
+        System.out.println(count);
+    }
+}
